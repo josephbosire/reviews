1165b328223116faf795e288f7406864e719df02
Joseph Bosire <josephbosire@josephs-mbp.home>
2024-09-12 16:35:30 -0400

    Add icons

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,5 +1,24 @@
+import { TriangleUpIcon } from "@radix-ui/react-icons";
+
 const FeedbackList = () => {
-  return <ol className="feedback-list"></ol>;
+  return (
+    <ol className="feedback-list">
+      <li className="feedback">
+        <button>
+          <TriangleUpIcon />
+          <span>593</span>
+        </button>
+        <div>
+          <p>B</p>
+        </div>
+        <div>
+          <p>Bytegrad</p>
+          <p>lorem ipsum asdcasdcasdcasdcasd</p>
+        </div>
+        <p>4d</p>
+      </li>
+    </ol>
+  );
 };
 
 export default FeedbackList;
